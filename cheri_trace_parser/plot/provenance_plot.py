59c2cc8c415607daba58fac8c14dda22f30768a9
qwattash <mzz.lrd@gmail.com>
2016-10-19 16:02:31 +0100

    Add BERI license headers

--- cheri_trace_parser/plot/provenance_plot.py
+++ cheri_trace_parser/plot/provenance_plot.py
@@ -1,4 +1,19 @@
 """
+Copyright 2016 Alfredo Mazzinghi
+
+Copyright and related rights are licensed under the BERI Hardware-Software
+License, Version 1.0 (the "License"); you may not use this file except
+in compliance with the License.  You may obtain a copy of the License at:
+
+http://www.beri-open-systems.org/legal/license-1-0.txt
+
+Unless required by applicable law or agreed to in writing, software,
+hardware and materials distributed under this License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.  See the License for the specific language governing
+permissions and limitations under the License.
+
+
 Plot representation of a CHERI pointer provenance tree
 """
 
