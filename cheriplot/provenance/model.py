CTSRD-CHERI/cheriplot

commit 6a29d2992980ed1a8173e5334c9d40e8300b862c

Split the provenance tool into separate tools for trace parsing, graph symbols resolution, plots and statistics.
Improve tools help messages.

cheriplot/provenance/model.py
@@ -539,6 +539,7 @@ class ProvenanceGraphManager:
         self.graph.ep["addr"] = prop_edge_address
         self.graph.ep["regs"] = prop_edge_regs
         self.graph.gp["stack"] = prop_initial_stack
+        self.graph.gp["name"] = prop_graph_name
 
         self._init_props()
 

cheriplot/core/driver.py
@@ -280,10 +280,11 @@ class SubCommand(DriverConfigEntry):
         nested_ns = NestingNamespace()
         setattr(ns, self.name, nested_ns)
         subparser = parser.add_subparsers()
+        self.kwargs["help"] = self.kwargs.get("help", self.nested.description)
         subcommand = subparser.add_parser(self.name, *self.args, **self.kwargs)
         # build a default value holding the subcommand class so that we do not
         # have to search for the subcommand that has been provided
-        subcommand_class_arg = "%ssubcommand_class" % prefix
+        subcommand_class_arg = "{}subcommand_class".format(prefix)
         def wrap_subcommand(*args, **kwargs):
             kwargs["config"] = getattr(kwargs["config"], self.name,
                                        kwargs.get("config", None))
@@ -291,12 +292,12 @@ class SubCommand(DriverConfigEntry):
         subcommand.set_defaults(**{subcommand_class_arg: wrap_subcommand})
         # make sure subparser namespace is created even with subcommands with
         # no arguments/options
-        prefix += "%s." % self.name
+        prefix += "{}.".format(self.name)
         model = self.nested.get_config_model()
         return model.make_config(subcommand, prefix=prefix, ns=nested_ns)
 
     def __str__(self):
-        return "<%s %s>" % (self.name, self.nested.get_config_model())
+        return "<{0} {1}>".format(self.name, self.nested.get_config_model())
 
 
 class DriverConfig(ProxyConfig):

cheriplot/provenance/__init__.py
@@ -1,4 +1,4 @@
 from .model import *
 from .parser import *
-from .driver import *
+from .driver import GraphAnalysisDriver
 from .graph_dump import *

cheriplot/provenance/driver.py
@@ -27,60 +27,48 @@
 
 import logging
 
-from cheriplot.core import SubCommand, BaseTraceTaskDriver, ProgressTimer, Option
+from cheriplot.core import SubCommand, BaseToolTaskDriver, Argument
 from cheriplot.provenance.plot import (
     AddressMapPlotDriver, AddressMapDerefPlotDriver, PtrSizeDerefDriver,
     PtrSizeBoundDriver, PtrSizeCdfDriver)
-from cheriplot.provenance.parser import CheriMipsModelParser
+from cheriplot.provenance.model import ProvenanceGraphManager
 from cheriplot.provenance.stats import ProvenanceStatsDriver
-from cheriplot.provenance.transforms import *
 from cheriplot.provenance.visit import (
     FilterNullAndKernelVertices, FilterCfromptr, MergeCfromptr)
 
 logger = logging.getLogger(__name__)
 
-__all__ = ("ProvenancePlotDriver",)
-
-class ProvenancePlotDriver(BaseTraceTaskDriver):
+class GraphAnalysisDriver(BaseToolTaskDriver):
     """
     Main task driver that registers all the other plot driver tools and gives them
     the provenance graph as input.
     """
+    description = """
+    Graph processing and plotting tool.
+    This tool processes a cheriplot graph to produce plots and statistics.
+    """
+
+    graph = Argument(help="Path to the cheriplot graph.")
     addrmap = SubCommand(AddressMapPlotDriver)
     addrmap_deref = SubCommand(AddressMapDerefPlotDriver)
     ptrsize_cdf = SubCommand(PtrSizeCdfDriver)
     ptrsize_bound = SubCommand(PtrSizeBoundDriver)
     ptrsize_deref = SubCommand(PtrSizeDerefDriver)
     stats = SubCommand(ProvenanceStatsDriver)
-    threads = Option(
-        type=int,
-        default=1,
-        help="Run the tool with the given number of workers")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._parser = CheriMipsModelParser(cache=self.config.cache,
-                                            trace_path=self.config.trace,
-                                            threads=self.config.threads)
+        self.pgm = ProvenanceGraphManager.load(self.config.graph)
+        """Loaded graph manager."""
 
     def run(self):
-        # XXX should probably change the way we encapsulte stuff here,
-        # this should be a simple task driver and each subcommand should inherit
-        # from a base driver that provides common arguments so that multiple
-        # traces can be handled more consistently.
-        self._parser.parse()
-        # get the parsed provenance graph model
-        pgm = self._parser.get_model()
-        # free the parser to reclaim memory
-        del self._parser
-
         # do the filtering of the graph here
-        filters = (FilterNullAndKernelVertices(pgm) +
-                   MergeCfromptr(pgm) +
-                   FilterCfromptr(pgm))
-        filtered_graph = filters(pgm.graph)
+        filters = (FilterNullAndKernelVertices(self.pgm) +
+                   MergeCfromptr(self.pgm) +
+                   FilterCfromptr(self.pgm))
+        filtered_graph = filters(self.pgm.graph)
         vfilt, _ = filtered_graph.get_vertex_filter()
-        pgm.graph.set_vertex_filter(vfilt)
+        self.pgm.graph.set_vertex_filter(vfilt)
 
-        sub = self.config.subcommand_class(pgm, config=self.config)
+        sub = self.config.subcommand_class(self.pgm, config=self.config)
         sub.run()

cheriplot/provenance/parser/__init__.py
@@ -1,3 +1,3 @@
 from .sub_mips import CheriMipsModelParser
 from .error import *
-from .driver import GraphParserDriver
+from .driver import GraphParserDriver, SymbolResolutionDriver

cheriplot/provenance/parser/driver.py
@@ -31,25 +31,31 @@ graph from a trace input.
 
 import logging
 
-from cheriplot.core import SubCommand, BaseTraceTaskDriver, ProgressTimer, Option
+from cheriplot.core import (
+    BaseToolTaskDriver, BaseTraceTaskDriver, Option, Argument, NestedConfig)
+from cheriplot.vmmap import VMMapFileParser
+from cheriplot.dbg.symbols import SymReader
 from cheriplot.provenance.parser import CheriMipsModelParser
 from cheriplot.provenance.model import ProvenanceGraphManager
+from cheriplot.provenance.visit import ResolveSymbolsGraphVisit
 
 logger = logging.getLogger(__name__)
-
-__all__ = ("GraphBuildDriver",)
-
 
 class GraphParserDriver(BaseTraceTaskDriver):
     """
     Task driver that generates a cheriplot graph from a trace file.
 
     Available parameters are:
-    
-    - :class:`BaseTraceTaskDriver` parameters
-    - threads: the number of threads to use (default 1)
-    - outfile: the output trace file (default <trace_file_name>_graph.gt
+
+    * :class:`BaseTraceTaskDriver` parameters
+    * threads: the number of threads to use (default 1)
+    * outfile: the output trace file (default <trace_file_name>_graph.gt
     """
+    description = """
+    Trace parse tool.
+    This tool generates a cheriplot graph from a CHERI trace.
+    """
+
     threads = Option(
         type=int,
         default=1,
@@ -76,6 +82,56 @@ class GraphParserDriver(BaseTraceTaskDriver):
     def run(self):
         self._parser.parse()
         # get the parsed provenance graph model
-        self.pgm.save(name=self.config.display_name)        
+        self.pgm.save(name=self.config.display_name)
         # force free the parser to reclaim memory
         del self._parser
+
+
+class SymbolResolutionDriver(BaseToolTaskDriver):
+    """
+    Task driver that fetches symbol names from binary and source files.
+
+    This step requires different input data:
+
+    * Output from procstat-like commands in csv or tab-separated format,
+      this is required to extract the base address of the sections of
+      a binary in memory.
+    * Binary ELF files containing the debug symbols.
+    * Kernel syscalls.master file to map syscall numbers to a name/signature.
+    """
+    description = """
+    Resolve call symbols in a cheriplot graph.
+    This is a postprocessing tool that extracts debug information from
+    ELF files, sources and runtime information to add symbol names and
+    call signatures to the cheriplot graph.
+
+    The tool is incremental, it can be run multiple times on the graph.
+    """
+
+    graph = Argument(help="Path to the cheriplot graph.")
+    vmmap = NestedConfig(VMMapFileParser)
+    elfpath = Option(
+        nargs="+",
+        default=[],
+        help="Paths where to look for ELF files with symbols")
+    syscalls = Option(
+        default=None,
+        help="Path to the syscalls.master file")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.pgm = ProvenanceGraphManager.load(self.config.graph)
+        """Loaded graph manager."""
+
+        self.vmmap = VMMapFileParser(config=self.config.vmmap)
+        """Memory map file parser."""
+
+        # self.syscalls = BSDSyscallMasterParser(self.config.syscalls)
+        # """Parser for the syscalls.master file."""
+
+    def run(self):
+        self.vmmap.parse()
+        # self.syscalls.parse()
+        visitor = ResolveSymbolsGraphVisit(self.pgm)
+        visitor(self.pgm.graph)
+        self.pgm.save()

cheriplot/provenance/plot/addr_map.py
@@ -448,6 +448,7 @@ class BaseAddressMapPlotDriver(VMMapPlotDriver, ASAxesPlotBuilderNoTitle):
     Plot that shows the capability size in the address space
     vs the time of allocation (i.e. when the capability is created).
     """
+
     title = "Capabilities derivation time vs capability position"
     x_label = "Virtual Address"
     y_label = "Time (million of cycles)"
@@ -494,6 +495,15 @@ class AddressMapPlotDriver(BaseAddressMapPlotDriver):
     Plot that shows the capability size in the address space
     vs the time of allocation (i.e. when the capability is created).
     """
+    description = """
+    Generate address-map plot.
+
+    The address-map plot shows the location and size of capabilities in
+    the address-space over time.
+    The Y axis shows elapsed time in cycles or committed instructions.
+    The X axis is a non-linear representation of the address space.
+    Capabilities are color coded according to the permission bits.
+    """
     title = "Capabilities derivation time vs capability position"
     patch_builder_class = ColorCodePatchBuilder
 
@@ -503,6 +513,15 @@ class AddressMapDerefPlotDriver(BaseAddressMapPlotDriver):
     Plot that shows the capability size in the address space
     vs the time of dereference (i.e. when the capability is dereferenced
     for a load or a store).
+    """
+    description = """
+    Generate address-map plot with dereference information.
+
+    The address-map plot shows the location and size of capabilities in
+    the address-space at the time of dereference.
+    The Y axis shows elapsed time in cycles or committed instructions.
+    The X axis is a non-linear representation of the address space.
+    Capabilities are color coded according to the permission bits.
     """
     title = "Capabilities deallocation time vs capability position"
     patch_builder_class = DerefPatchBuilder

cheriplot/provenance/visit/__init__.py
@@ -2,4 +2,6 @@
 This module implements the various algorithms that walk the graph to filter, update or aggregate data.
 """
 from .base import *
-from .vertex_merge import *
+from .vertex_merge import (
+    MaskBFSVisit, FilterNullAndKernelVertices, FilterCfromptr, MergeCfromptr)
+from .symbols import ResolveSymbolsGraphVisit

commit ed637592c3a72523d995d7c41642cc1521873d97

Fix typos in graph model load introduced by 6a29d29929

cheriplot/provenance/model.py
@@ -500,8 +500,9 @@ class ProvenanceGraphManager:
         """
         pgm = cls(source)
         with ProgressTimer("Load cached graph", logger):
-            self.graph = load_graph(cache_file)
-        self._init_props()
+            pgm.graph = load_graph(source)
+        pgm._init_props()
+        return pgm
 
     def __init__(self, outfile):
         """